1ff820a3492910c982a7704815511aa20f5c649c
tarikaziz0933 <tarikaziz0933@gmail.com>
2023-10-11 19:24:34 +0600

    addeded order api

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../context/UserContext';
 
 const CheckOut = () => {
-    const { service_id } = useLoaderData();
+    const { _id, title, price } = useLoaderData();
+    const { user } = useContext(AuthContext);
+
+    const handlePlaceOrder = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = `${form.firstName.value} ${form.lastName.value}`;
+        const phone = form.phoneNumber.value;
+        const message = form.message.value;
+        const email = user?.email || 'unregistered';
+
+        const order = {
+            service: _id,
+            serviceName: title,
+            price,
+            customer: name,
+            email,
+            phone,
+            message
+
+        }
+
+        if (phone.length < 10) {
+            alert('Phone No. should be 10 characters longer');
+        }
+        else {
+            fetch('http://localhost:5000/orders', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(order)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.acknowledged) {
+                        alert('Order placed successfully')
+                        form.reset();
+                    }
+                })
+                .catch(err => console.error(err));
+
+        }
+    }
+
+
     return (
         <div>
-            <h2>Checkout</h2>
-            <p>{service_id}</p>
+            <form onSubmit={handlePlaceOrder}>
+                <h2 className="text-4xl">Your Order: {title}</h2>
+                <h4 className="text-3xl">Price: {price}</h4>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                    <input type="text" name='firstName' placeholder="First Name" className="input input-bordered w-full" />
+                    <input type="text" name='lastName' placeholder="Last Name" className="input input-bordered w-full" />
+                    <input type="text" name='phoneNumber' placeholder="Phone Number" className="input input-bordered w-full" required />
+                    <input type="text" name='email' placeholder="Your Email" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
+                </div>
+                <textarea name='message' className="textarea textarea-bordered h-20 w-full my-6" placeholder="Messge"></textarea>
+                <input className='btn' type="submit" value="Place Your Order" />
+            </form>
         </div>
     );
 };
--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -5,7 +5,7 @@ const ServiceCard = ({ service }) => {
     const { _id, img, title, price } = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
+            <figure><img className='' src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>Price: {price}</p>
